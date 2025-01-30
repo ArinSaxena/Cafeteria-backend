33d41165fcecef79f1a7732a4ffc994f8475bfcc
Arin Saxena <arinsaxena0002@gmail.com>
2025-01-31 05:18:07 +0530

    authorization

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -99,8 +99,13 @@ const logout = (req, res) => {
 
 const getUser= async (req, res) => {
   const id = req.params.id;
+  console.log(id);
   try {
     const user = await User.findById(id).select("-password");
+    console.log(user);
+    if(!user){
+      return res.status(404).json({message:"User not found!"})
+    }
     res.json(user);
   } catch (err) {
     res.status(500).json({ error: "Error fetching users" });
