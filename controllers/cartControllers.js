07a67967cf7802b7b10d6728c791055968764491
Arin Saxena <arinsaxena0002@gmail.com>
2025-02-06 06:40:55 +0530

    quantiy

--- controllers/cartControllers.js
+++ controllers/cartControllers.js
@@ -21,29 +21,30 @@ const saveCart = async (req, res) => {
   }
   req.user.cartItems.push({dish:dishId, quantity });   
   await req.user.save().then((u) => u.populate("cartItems.dish"));
-  // console.log("qwer",req.user.cartItems)
   res.status(201).json(req.user.cartItems);
   // console.log(req.user.cartItems);
 };
 
 const updateCart = async (req, res) => {
   const {dishId } = req.params;
-  const {quantity} = req.body;
+  // const {quantity} = req.body;
+  const {action}= req.body;
   try{
-    // const id = "6795d16762f2193673636635";
     const id = req.user._id;
 
     const user = await User.findById(id).populate("cartItems.dish");
   
-  if(!quantity || quantity <1){
-    return res.status(400).json({ error: "Quantity must be at least 1." });
-
-  }
-  const item = user.cartItems.find((item) => item._id === dishId);
+  const item = user.cartItems.find((item) => item._id === dishId);              // id comparison problem
   if (!item) {
     return res.status(404).json({ error: "Item not found in cart." });
   }
-  item.quantity += quantity; // Update the quantity
+  if(action === "increment"){
+    item.quantity +=1;
+  }
+  else {
+      item.quantity -=1;
+  }
+
   await user.save(); // Save the updated user document
 
   res.status(200).json({ message: "Cart item updated.", cart: user.cartItems });
