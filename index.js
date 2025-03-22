f8ad63391590d0d9a545824d873b26d4cae513e2
Arin Saxena <arinsaxena0002@gmail.com>
2025-03-22 16:12:39 +0530

    modularizing env

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-require('dotenv').config();
 require('./config/connectiondb')
 const express =  require('express');
-const mongoose =  require('mongoose');
-const bcrypt =  require('bcrypt');
-const multer = require('multer');
 const cors = require('cors');
 
 const userRouter = require('./routes/userRoutes');
@@ -11,19 +7,19 @@ const dishRouter = require('./routes/dishRoutes');
 const counterRouter = require('./routes/counterRoutes');
 const cartRouter = require('./routes/cartRoutes');
 const authRoutes = require('./routes/authRoutes');
-const path = require("path")
+const path = require("path");
+const { FRONTEND_URL, PORT } = require('./config/envConfig');
 
 const app = express();
 
 app.use(express.json());
 app.use(
     cors({
-      origin: "http://localhost:3000", 
+      origin: FRONTEND_URL, 
       allowedHeaders: ["Content-Type", "Authorization"], 
     })
   );
   
-  // Handle preflight requests properly
   app.options("*", cors()); // Respond to preflight requests
   
 
@@ -31,14 +27,12 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 
 app.use("/auth", authRoutes);
-
 app.use("/users", userRouter);
 app.use("/dishes", dishRouter);
 app.use("/counter", counterRouter);
 app.use("/cart", cartRouter);
 
 
-const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
